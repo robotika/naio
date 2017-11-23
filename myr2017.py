4a2ee03f46ea9d0b36be820ee02f6ebfedeafc07
Martin Dlouhy <md@robotika.cz>
2017-11-23 06:16:21 +0100

    Fix automatic generation of log filename

--- myr2017.py
+++ myr2017.py
@@ -3,6 +3,8 @@
 # Echo client program
 import socket
 import sys
+import datetime
+
 
 HOST = '127.0.0.1'    # The remote host
 PORT = 5559              # The same port as used by the server
@@ -27,7 +29,8 @@ if s is None:
 with s:
     # commad to drive motors
     s.sendall(b'NAIO01\x01\x00\x00\x00\x02\x70\x70\xCD\xCD\xCD\xCD')
-    f = open('data171122.bin', 'wb')
+    filename = datetime.datetime.now().strftime("naio%y%m%d_%H%M%S.log")
+    f = open(filename, 'wb')
     for i in range(1000):
         data = s.recv(1024)
         assert len(data) > 7 and data[:6] == b'NAIO01', data
